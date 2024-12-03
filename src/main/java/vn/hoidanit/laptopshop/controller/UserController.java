e54f312a906ec6fe61485773df22f4d5d98ad205
thTrong0510 <vthanhtrongng@gmail.com>
2024-12-03 14:52:16 +0700

    quay lai mo hinh MVC bo Rest API, render view tinh html = tao file tinh html trong source dung controller dieu huong den file nay de hien thi

--- src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
+++ src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
@@ -5,23 +5,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import vn.hoidanit.laptopshop.service.UserService;
+import org.springframework.web.bind.annotation.RequestMapping;
 
-// @Controller
-// public class UserController {
-
-//     private UserService userService;
-
-//     public UserController(UserService userService) {
-//         this.userService = userService;
-//     }
-
-//     @RequestMapping("")
-//     public String getHomaPage() {
-//         return this.userService.handleGetHomePage();
-//     }
-// }
-
-@RestController
+@Controller
 public class UserController {
 
     private UserService userService;
@@ -30,8 +16,23 @@ public class UserController {
         this.userService = userService;
     }
 
-    @GetMapping("")
+    @RequestMapping("")
     public String getHomaPage() {
-        return this.userService.handleGetHomePage();
+        return "hello.html";
     }
 }
+
+// @RestController
+// public class UserController {
+
+// private UserService userService;
+
+// public UserController(UserService userService) {
+// this.userService = userService;
+// }
+
+// @GetMapping("")
+// public String getHomaPage() {
+// return this.userService.handleGetHomePage();
+// }
+// }
