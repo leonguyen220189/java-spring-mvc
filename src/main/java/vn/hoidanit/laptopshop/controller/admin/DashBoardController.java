6de44601af56f18f32567e4188d3b3594fad58e5
thTrong0510 <vthanhtrongng@gmail.com>
2024-12-07 15:31:01 +0700

    completed admin page follow to a template

---
+++ src/main/java/vn/hoidanit/laptopshop/controller/admin/DashBoardController.java
@@ -0,0 +1,16 @@
+package vn.hoidanit.laptopshop.controller.admin;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import ch.qos.logback.core.model.Model;
+
+@Controller
+public class DashBoardController {
+
+    @GetMapping("/admin")
+    public String getAdminPage(Model model) {
+        return "admin/dashboard/show_admin_page";
+    }
+}
